c3aa123fef1febb32d1e1292f472323cfadf268b
action <action@github.com>
2025-03-28 04:46:42 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Mar 28 2025 03:23:44 GMT+0000 (Coordinated Universal Time)
+//Fri Mar 28 2025 04:46:42 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 let MAX_AUTH_RETRY = 3;
